3de5e556d3a7c143f9588491f46b2b4946aa81ae
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-07-18 15:22:23 -0400

    feat: basic Player class created

--- public/game/player.js
+++ public/game/player.js
@@ -1,1 +1,20 @@
 // player.js
+
+// imports
+import { Gameboard } from "./gameboard.js";
+
+const message = "Successfully linked player";
+
+// two types of players in the game, ‘real’ players and ‘computer’ players
+// each player object should contain its own gameboard
+
+// player class constructor
+class Player {
+  constructor(name, type) {
+    this.name = name; // player name
+    this.type = type; // human or computer
+  }
+}
+
+// exports
+export { message, Player };
--- tests/player.test.js
+++ tests/player.test.js
@@ -1,1 +1,10 @@
 // player.test.js
+
+import { message, Player } from "../public/game/player.js";
+
+test("player module imports correctly", () => {
+  expect(message).toBe("Successfully linked player"); // check file is linked
+});
+
+// two types of players in the game, ‘real’ players and ‘computer’ players
+// each player object should contain its own gameboard
